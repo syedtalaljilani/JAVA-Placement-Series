c360cb348039037c51044eb2cf163b397441ca7a
syedtalaljilani <talaljilani79@gmail.com>
2022-08-28 19:36:10 +0500

    int 32 bit datatype size print and string datatype

--- Programming Fundamental/helloworld.cpp
+++ Programming Fundamental/helloworld.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+{  
+    int32_t a ;
+    cout << sizeof(a) << endl; 
+    string s ="HelloWorld";
+    cout << s << endl;
 }
