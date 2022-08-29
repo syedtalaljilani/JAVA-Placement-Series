2bdcb882d00aa0299efe93aa57f1b67b78034f79
syedtalaljilani <talaljilani79@gmail.com>
2022-08-29 22:49:23 +0500

    datatype.cpp is added

---
+++ Programming Fundamental/datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){ 
+    int a=10; // integer datatype
+    float b=10.0f; // float datatype
+    string c="Hello World"; // string datatype
+    char d = 'A'; // character datatype
+    long e = 10l; // l stand for long  (long datatype)
+    double f = 120.897; // double datatype
+    unsigned g = 1234; // unsigned int datatype
+    signed h = -2343; // signed int datatype
+    int i = 12.09l; // long double datatype 
+    
+    // Print all variable using cout function
+    
+}
